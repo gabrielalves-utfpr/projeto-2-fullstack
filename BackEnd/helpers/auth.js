9ce33de1b9a818ea1d4f6373f48c3d3a1a945f48
gabrielalves-utfpr <gabrielalvessantos@alunos.utfpr.edu.br>
2023-12-07 20:00:25 -0300

    fixing websocket token

--- BackEnd/helpers/auth.js
+++ BackEnd/helpers/auth.js
@@ -29,29 +29,32 @@ module.exports = {
         }
     },
     authenticateWS: async function(req, res, next){
-        let reqbearer = req.headers['authorization']
-        if (reqbearer != null && reqbearer != undefined){
+        try {
+          const url = require('url');
+          // analisar o req.url e obter o token da query string
+          let reqbearer = url.parse(req.url, true).query.authorization;
+          if (reqbearer != null && reqbearer != undefined){
             let bearer = reqbearer.split(' ')
             if (bearer[0] != 'Bearer') {
-                return res.status(400).json({status:false, mensagem:"Não à Bearer"})
+              throw {status:false, mensagem:"Não à Bearer"}
             }
-            jwt.verify(bearer[1], process.env.SECRET, (erro, user) =>{
-                if(erro){
-                        throw {status:false, mensagem:erro.name}
-                }
-                req.user = user
-            })
+            const user = await jwt.verify(bearer[1], process.env.SECRET);
+            req.user = user;
             if(req.user != null){
-                let user = await UserModel.isRegistered({username: req.user.username, password: req.user.password})
-                if (user != null){
-                    req.user.id = user.id
-                    next()
-                }else{
-                    throw {status:false, mensagem:'Credenciais erradas'}
-                }
+              let user = await UserModel.isRegistered({username: req.user.username, password: req.user.password})
+              if (user != null){
+                req.user.id = user.id
+                next()
+              }else{
+                throw {status:false, mensagem:'Credenciais erradas'}
+              }
             }
-        }else {
+          }else {
             throw {status:false, mensagem:'Credenciais não encontradas'}
+          }
+        } catch (err) {
+          console.log(err);
+          next(err);
         }
-    },
+      },
 }
